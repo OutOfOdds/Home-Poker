85aaaf5b315e38e7670edd2283612de54bfa76dc
Mike Mailyan <Mihomailian@gmail.com>
2025-10-23 21:23:06 +0300

    Небольшие улучшения

--- Home Poker/App/HomePokerApp.swift
+++ Home Poker/App/HomePokerApp.swift
@@ -7,7 +7,7 @@ struct Home_PokerApp: App {
     @State var sessionDetailVM = SessionDetailViewModel()
     var body: some Scene {
         WindowGroup {
-            SessionListView()
+            MainView()
                 .environment(sessionDetailVM)
         }
         .modelContainer(for: [Player.self, Session.self, PlayerTransaction.self, Expense.self, SessionBank.self, SessionBankTransaction.self])
---
+++ Home Poker/Features/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  Home Poker
+//
+//  Created by Odds on 23.10.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            Tab("Сессии", systemImage: "list.star") {
+                SessionListView()
+            }
+            Tab("Таймер", systemImage: "timer") {
+                Text("В разработке 🚧")
+                    .monospaced()
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
